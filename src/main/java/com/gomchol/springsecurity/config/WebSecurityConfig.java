b0e8e278c2695af84f6ec561babf47fc0c89a1ae
Myungcheol Doo <myungcheol@gmail.com>
2018-11-09 11:36:38 -0600

    Modify security settings
    - remove Unauthorized status entry point
    - add /private rule and BasicAuth

--- src/main/java/com/gomchol/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/gomchol/springsecurity/config/WebSecurityConfig.java
@@ -1,25 +1,22 @@
 package com.gomchol.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.exceptionHandling()
-                .authenticationEntryPoint(
-                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
-                );
-
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated();
+                .antMatchers("/private").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                ;
     }
 }
